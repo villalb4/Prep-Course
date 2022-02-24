580b810b1e2cb033a5d4976a10317a28b716d1c2
villalb4 <villalb4angel@gmail.com>
2022-02-24 17:48:19 -0300

    Completado

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -118,6 +118,10 @@ function pasarUsuarioAPremium(usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
+  for (var i = 0 ; i < usuarios.length ; i ++) {
+    usuarios[i].esPremium = true;
+  }
+  return usuarios;
 }
 
 function sumarLikesDeUsuario(usuario) {
@@ -127,6 +131,11 @@ function sumarLikesDeUsuario(usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  var contador = 0;
+  for (var i = 0 ; i < usuario.posts.length ; i ++) {
+    contador += usuario.posts[i].likes
+  }
+  return contador;
 }
 
 function agregarMetodoCalculoDescuento(producto) {
@@ -139,7 +148,10 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-
+  producto.calcularPrecioDescuento = function() {
+    return producto.precio - (producto.precio * producto.porcentajeDeDescuento)
+  }
+  return producto;
 }
 
 // No modificar nada debajo de esta línea
