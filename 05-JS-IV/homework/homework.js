b1beed684d5aa6758f5fba6433a5b95b689b4417
villalb4 <villalb4angel@gmail.com>
2022-02-24 18:37:49 -0300

    Completado

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -149,7 +149,7 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
   producto.calcularPrecioDescuento = function() {
-    return producto.precio - (producto.precio * producto.porcentajeDeDescuento)
+    return this.precio - (this.precio * this.porcentajeDeDescuento)
   }
   return producto;
 }
